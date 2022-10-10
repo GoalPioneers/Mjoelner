8b5f1b7b5cbc0e4f6eeb246079e3ade74c73f18d
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-10-10 09:49:25 +0200

    added composer

--- debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/usr/bash
-valac --pkg gtk+-3.0 ./src/app.vala -o ./bin/app.so
-
-./bin/app.so
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'io-jaegers/mjoelner',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'abfdef42f6df44b5e741030028cf76902887fe59',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'io-jaegers/mjoelner' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'abfdef42f6df44b5e741030028cf76902887fe59',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
